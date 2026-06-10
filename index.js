5b635a6bbe92df704a4a3c74a7d105933996c782
shubham-Alhat <shubhamalhat520@gmail.com>
2026-06-10 20:49:24 +0530

    write some sql queries to have hands dirty with it

--- index.js
+++ index.js
@@ -11,7 +11,19 @@ const pool = new Pool({
 
 app.use(express.json());
 
-app.post("/signup", (req, res) => {});
+app.post("/signup", async (req, res) => {
+  const username = req.body.username;
+  const email = req.body.email;
+  const password = req.body.password;
+
+  const user = await pool.query(
+    `SELECT * FROM users WHERE username = '${username}' AND password = '${password}'`,
+  );
+
+  console.log(user);
+
+  return res.json({ message: "login success." });
+});
 
 app.post("/login", async (req, res) => {
   const username = req.body.username;
@@ -28,17 +40,19 @@ app.post("/login", async (req, res) => {
       "')",
   );
 
-  await pool.query(
+  const user = await pool.query(
     "INSERT INTO users (username,email,password) VALUES ('" +
       username +
       "','" +
       email +
       "','" +
       password +
-      "')",
+      "') RETURNING id",
   );
 
-  res.json({ message: "user login success...!!" });
+  console.log(user);
+
+  res.json({ message: "user login success...!!", userId: user.rows[0].id });
 });
 
 app.get("/", (req, res) => {
